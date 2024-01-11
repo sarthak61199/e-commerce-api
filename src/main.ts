72f2fc973f781753151288eb5023a32c20e35e77
Sarthak Agarwal <sarthakroxxx99@gmail.com>
2024-01-12 00:58:22 +0530

    updated cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: 'http://localhost:5173',
+      origin: [
+        'http://localhost:5173',
+        'https://e-commerce-react-one-eosin.vercel.app/',
+      ],
       credentials: true,
     },
   });
